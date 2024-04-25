ba9f8d750d2562daa74ff65c6fcd0276fecccae9
agilistikmal <agilistikmal3@gmail.com>
2024-04-25 08:06:37 +0700

    Initial

---
+++ src/app/(backend)/api/admin/login/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+// TODO: implement me
+export async function GET() {
+  return NextResponse.json({
+    status: 200,
+    error: null,
+    data: null,
+  });
+}
---
+++ src/app/(frontend)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">Hello World</h1>
+    </div>
+  );
+}
